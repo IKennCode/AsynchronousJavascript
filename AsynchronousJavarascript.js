3b444e1da044344f272603744cb80be938336438
Kenneth Dimalibot <dimalibotkennethofficial@gmail.com>
2023-11-22 21:36:59 +0800

    asd

--- AsynchronousJavarascript.js
+++ AsynchronousJavarascript.js
@@ -12,59 +12,65 @@
 
 //Callback
 
+// let stocks = {
+//     Fruits : ["mangga", "papaya", "strawberry", "coconut"],
+//     liquid : ["water", "ice"],
+//     holder : ["cone", "cup", "stick"],
+//     toppings : ["chocolate", "peanuts"]
+// }
+
+
+
+// let order = (Fruits_name, call_production) => {
+   
+//     setTimeout(() => {
+       
+//         console.log(`${stocks.Fruits[Fruits_name]} was selected`)
+
+//         call_production()
+    
+
+//     }, 2000)
+  
+// }
+
+
+// let production = () => {
+
+//     setTimeout(() => {
+//         console.log("Production has started")
+//         setTimeout(() => {
+//             console.log("the fruit has been chopped")
+//             setTimeout(() => {
+//                 console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`)
+//                 setTimeout(() => {
+//                     console.log("the machine was started")
+//                     setTimeout(() => {
+//                         console.log(`${stocks.holder[0]} was selected to be container`)
+//                         setTimeout(() => {
+//                             console.log(`${stocks.toppings[0]} was selected to be toppings`)
+//                             setTimeout(() => {
+//                                 console.log("Served Icecream")
+//                             }, 3000)
+//                         }, 3000)
+//                     }, 1000)
+//                 }, 1000)
+//             }, 1000)
+//         }, 2000)
+//     }, 1000)
+  
+// }
+
+// order(1, production)
+
+//Promises
+
 let stocks = {
     Fruits : ["mangga", "papaya", "strawberry", "coconut"],
     liquid : ["water", "ice"],
     holder : ["cone", "cup", "stick"],
     toppings : ["chocolate", "peanuts"]
 }
-
-
-
-let order = (Fruits_name, call_production) => {
-   
-    setTimeout(() => {
-       
-        console.log(`${stocks.Fruits[Fruits_name]} was selected`)
-
-        call_production()
-    
-
-    }, 2000)
-  
-}
-
-
-let production = () => {
-
-    setTimeout(() => {
-        console.log("Production has started")
-        setTimeout(() => {
-            console.log("the fruit has been chopped")
-            setTimeout(() => {
-                console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`)
-                setTimeout(() => {
-                    console.log("the machine was started")
-                    setTimeout(() => {
-                        console.log(`${stocks.holder[0]} was selected to be container`)
-                        setTimeout(() => {
-                            console.log(`${stocks.toppings[0]} was selected to be toppings`)
-                            setTimeout(() => {
-                                console.log("Served Icecream")
-                            }, 3000)
-                        }, 3000)
-                    }, 1000)
-                }, 1000)
-            }, 1000)
-        }, 2000)
-    }, 1000)
-
-   
-  
-}
-
-
-order(1, production)
 
 
 
